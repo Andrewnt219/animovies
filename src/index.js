de437a0f70b45a94712d95a799f6f000a23d7420
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-05-31 13:50:09 -0400

    ready to deploy

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import App from 'App';
 import store from 'App/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter basename="/">
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
