f978a16a2d760b9ee8d3afb6cfcef350d2116344
Tuan Phong Nguyen <phongnguyentuan20@gmail.com>
2020-05-31 13:49:13 -0400

    Updates

---
+++ precache-manifest.cc53a41e7ad3ee077b62937ea0cef00a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae56960eac5f26005550d306c487ec01",
+    "url": "/animovies/index.html"
+  },
+  {
+    "revision": "1a99bfc1b1c2170a3c35",
+    "url": "/animovies/static/css/main.6355ea8a.chunk.css"
+  },
+  {
+    "revision": "24981d910894f8200187",
+    "url": "/animovies/static/js/2.41260886.chunk.js"
+  },
+  {
+    "revision": "d2793e4227ba8a4cd50ba1b0cc92b018",
+    "url": "/animovies/static/js/2.41260886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a99bfc1b1c2170a3c35",
+    "url": "/animovies/static/js/main.f4af49c2.chunk.js"
+  },
+  {
+    "revision": "a128522ab26e02833922",
+    "url": "/animovies/static/js/runtime-main.1f247ad6.js"
+  },
+  {
+    "revision": "25af6611f3a52c545c3c0d9c5ea9b214",
+    "url": "/animovies/static/media/bg.25af6611.jpg"
+  }
+]);
